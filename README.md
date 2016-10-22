a5b056df7219c158ec4c2dae714883f0aba77728
Wells Riley <wells.riley@gmail.com>
2016-10-22 14:51:07 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,51 @@
+# homebridge-mpower
+This is a plugin for [homebridge](https://github.com/nfarina/homebridge). It allows you to control your Ubiquiti mPower outlets with HomeKit.
+
+# Installation
+
+1. Install homebridge (if not already installed) using: `npm install -g homebridge`
+2. Install this plugin using: `npm install -g homebridge-mpower`
+3. Update your configuration file. See below for a sample.
+
+# Configuration
+
+```
+"platforms": [
+  {
+    "platform": "mPower",
+    "name": "mPower",
+    "outlets": [
+      {
+        "name": "Fan",
+        "username": "admin",
+        "password": "hunter2",
+        "url": "10.0.1.5",
+        "id": "1"
+      },
+      {
+        "name": "Hall Light",
+        "username": "admin",
+        "password": "hunter2",
+        "url": "10.0.1.5",
+        "id": "2"
+      },
+      {
+        "name": "Mr. Coffee",
+        "username": "admin",
+        "password": "Correct Horse Battery Staple",
+        "url": "10.0.1.6",
+        "id": "1"
+      }
+    ]
+  }
+]
+```
+
+- `name` is the human-readable name of the device plugged into your outlet
+- `username` is your mFi Controller username
+- `password` is your mFi Controller password
+- `url` may be either a hostname or an IP address
+- `id` is the specific outlet you hope to control. For mPower mini, this can only be `1`. For mPower and mPower PRO, you might have to do some trial and error to figure out which outlet has which `id`. I only have an mPower mini, so I can't check :)
+
+# How it works
+This plugin is basically a homebridge-compatible implementation of the [Ubiquiti mFi outlet HTTP API](https://community.ubnt.com/t5/mFi/mPower-mFi-Switch-and-mFi-In-Wall-Outlet-HTTP-API/td-p/1076449). It sends a HTTP request via `curl` to your mFi outlet device (not the controller) to manually toggle the device `on` or `off`.
---
+++ index.js
@@ -0,0 +1,147 @@
+var Service, Characteristic;
+
+module.exports = function(homebridge) {
+  Service = homebridge.hap.Service;
+  Characteristic = homebridge.hap.Characteristic;
+
+  homebridge.registerPlatform("homebridge-mpower", "mPower", mPowerPlatform);
+}
+
+function mPowerPlatform(log, config){
+  function stringGen(length) {
+    var text = "";
+    var charset = "0123456789";
+    for( var i=0; i < length; i++ )
+        text += charset.charAt(Math.floor(Math.random() * charset.length));
+    return text;
+  }
+
+  this.log = log;
+  this.airos_sessionid = config["airos_sessionid"] || stringGen(32);
+  this.outlets = config["outlets"];
+}
+
+mPowerPlatform.prototype = {
+  accessories: function(callback){
+    var foundAccessories = [];
+
+    var count = this.outlets.length;
+
+    for(index=0; index< count; ++index){
+      var accessory  = new mPowerAccessory(
+        this.log,
+        this.airos_sessionid,
+        this.outlets[index]);
+
+      foundAccessories.push(accessory);
+    }
+
+    callback(foundAccessories);
+  }
+};
+
+function mPowerAccessory(log, airos_sessionid, outlet) {
+  this.log = log;
+  this.airos_sessionid = airos_sessionid;
+  this.name = outlet["name"],
+  this.username = outlet["username"];
+  this.password = outlet["password"];
+  this.url = outlet["url"];
+  this.id = outlet["id"];
+
+  this.services = [];
+
+  this.outletService = new Service.Outlet(this.name);
+
+  this.outletService
+    .getCharacteristic(Characteristic.On)
+    .on('get', this.getState.bind(this))
+    .on('set', this.setState.bind(this));
+
+   this.outletService
+    .getCharacteristic(Characteristic.OutletInUse)
+    .on('get', this.getState.bind(this))
+    .setValue(this.state === 'on');
+
+  this.services.push(this.outletService);
+}
+
+mPowerAccessory.prototype.setState = function(state, callback) {
+  var exec = require('child_process').exec;
+
+  var cmdAuth = 'curl -X POST -d "username=' + this.username + '&password=' + this.password + '" -b "AIROS_SESSIONID=' + this.airos_sessionid + '" ' + this.url + '/login.cgi';
+  var cmdUpdate = 'curl -X POST -d "output=' + state+ '" -b "AIROS_SESSIONID=' + this.airos_sessionid + '" ' + this.url + '/sensors/' + this.id;
+
+  var stateName = (state == 1) ? 'on' : 'off';
+
+  this.log("Turning " + this.name + " outlet " + stateName + ".");
+
+  exec(cmdAuth, function(error, stdout, stderr) {
+    if (!error) {
+      exec(cmdUpdate, function(error, stdout, stderr) {
+        if (!error) {
+          if (stdout != "") {
+            var response = JSON.parse(stdout);
+
+            if(response.status == "success") {
+              callback(null);
+            } else {
+              callback(error);
+            }
+          } else {
+            console.log("Failed with error: " + error);
+          }
+        }
+      });
+    } else {
+      console.log("Failed with error: " + error);
+    }
+  });
+}
+
+mPowerAccessory.prototype.getState = function(callback) {
+  var exec = require('child_process').exec;
+
+  var cmdAuth = 'curl -X POST -d "username=' + this.username + '&password=' + this.password + '" -b "AIROS_SESSIONID=' + this.airos_sessionid + '" ' + this.url + '/login.cgi';
+  var cmdStatus = 'curl -b "AIROS_SESSIONID=' + this.airos_sessionid + '" ' + this.url + '/sensors/' + this.id + '/output';
+
+  exec(cmdAuth, function(error, stdout, stderr) {
+    if (!error) {
+      exec(cmdStatus, function(error, stdout, stderr) {
+        if (!error) {
+          if (stdout != "") {
+            var state = JSON.parse(stdout);
+            if(state.sensors[0].output == 1) {
+              callback(null, true);
+            } else if(state.sensors[0].output == 0) {
+              callback(null, false);
+            }
+            else {
+              callback(error);
+            }
+          } else {
+            console.log("Failed to communicate with mPower accessory");
+          }
+        } else {
+          console.log("Failed with error: " + error);
+        }
+      });
+    } else {
+      console.log("Failed with error: " + error);
+    }
+  });
+}
+
+mPowerAccessory.prototype.getDefaultValue = function(callback) {
+  callback(null, this.value);
+}
+
+mPowerAccessory.prototype.setCurrentValue = function(value, callback) {
+  this.log("Value: " + value);
+
+  callback(null, value);
+}
+
+mPowerAccessory.prototype.getServices = function() {
+  return this.services;
+}
